c581eb44853b01363c93e669c3d38a6b25eddeaf
DEV09 <dev09@ccdenovl.intra>
2019-04-08 13:13:15 +0200

    Login gedaan

---
+++ js/login.js
@@ -0,0 +1,56 @@
+// Initialize
+window.addEventListener('load', function() {
+
+    // Gebruiker is ingelogd, naar profiel
+    if (IsIngelogd()) {
+        window.location.replace('profiel.html');
+        return;
+    }
+
+    var eLogin = document.querySelector('#login')
+    eLogin.addEventListener('click', OnLoginClick);
+
+    function OnLoginClick(evt) {
+        console.log("click")
+        evt.preventDefault();
+        let nickname =  document.getElementById('username').value; 
+        let wachtwoord =  document.getElementById('password').value;
+
+            let url = ROOT_URL + '/profiel/authenticate.php';
+
+            let data = {
+                nickname: nickname,
+                wachtwoord: wachtwoord
+            }
+
+            var request = new Request(url, {
+                method: 'POST',
+                body: JSON.stringify(data),
+                headers: new Headers({
+                    'Content-Type': 'application/json'
+                })
+            });
+            
+            fetch(request)
+            .then( function (resp) {return resp.json()})
+            .then( function (data) {
+                console.log(data) 
+                // Geeft ID terug als het iets vind
+                if (data.id) {
+                    OnLoginSuccess(data)
+                } else {
+                    OnLoginFail()
+                }
+
+            })
+    }
+
+    function OnLoginFail() {
+        console.log("Kan niet inloggen")
+    }
+
+    function OnLoginSuccess(data) {
+        // Local storage
+        localStorage.setItem('user_id', data.id)
+    }
+})
--- js/util.js
+++ js/util.js
@@ -1,5 +1,12 @@
+ROOT_URL = "https://scrumserver.tenobe.org/scrum/api";
 
 // Kijken of een gebruiker is ingelogd
 function IsIngelogd() {
-    return true;
+    return (localStorage.getItem('user_id') !== null);
 }
+
+// Logt een gebruiker uit
+function Logout() {
+    localStorage.removeItem('user_id')
+}
+
