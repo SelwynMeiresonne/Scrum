eabdbb3a97842821d6632bfcc739eabeaea5bb78
Selwyn Meiresonne <selwyn.meiresonne@hotmail.com>
2019-04-09 22:35:05 +0200

    Messenger changes

--- js/messenger.js
+++ js/messenger.js
@@ -1,5 +1,5 @@
 // Global functions
-
+TEST = {}
 // Called when the document is ready
 $('document').ready(function () {
     // Gebruiker is niet ingelogd, naar index
@@ -12,35 +12,40 @@ $('document').ready(function () {
     let url = ROOT_URL + "/bericht/read.php"
 
     let id = '10'
+    let current_id = 10
 
     let data = {
         profielId: 1,
     }
 
     let users = {}
+    let messages = []
     fetch(url + "?profielId=" + id)
         .then(function (response) { return response.json(); })
         .then(function (data) {
+            console.log("DATA -> ", data)
             data.forEach(element => {
-                console.log(element)
+                messages.push(element)
                 // Voor ieder bericht
-                for (let i = 0; i < element.length; i++) {
+                for (let i = 0; i < element.length - 1; i++) {
                     // Get the profiles
-                    GeefProfielVanID(element[i].naarId).then(function (data) {
-                        // Store it for later
-                        users[element[i].naarId] = data
+
+                    console.log(element[i].naarId == GeefGebruikerID() ?  element[id].vanId : element[id].naarId)
+                   //var id = element[i].naarId == GeefGebruikerID() ?  element[id].vanId : element[id].naarId
+            
+                    if (element[i].naarId != current_id) {
+                    GeefProfielVanID(id).then(function (data) {
+
+                        $('#contacten').append('<p>' + data.nickname + ' - ' + data.id + '</p>')
                     })
+                }
                 }
             })
 
-            // Call this when done
-            PopulateUserList(users)
         })
 
     // Called when profiles are in
     function PopulateUserList(users) {
-        let v = Object.entries(users)
-        console.log(v)
     }
 
 })
