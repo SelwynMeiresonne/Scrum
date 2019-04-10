1f98e6f04041e310e8c8942bdbb99a9665f42ac7
DEV09 <dev09@ccdenovl.intra>
2019-04-10 10:59:54 +0200

    Berichten basis

--- js/messenger.js
+++ js/messenger.js
@@ -33,40 +33,61 @@ $('document').ready(function () {
         })
 
     function CreateConversations(data) {
+        
+        let i = 0;
         data.forEach(element => {
+            console.log(element)
             let from_user = GeefGebruikerID()
             let to_user = GetChatOther(element.vanId, element.naarId)
+
+            console.log(' => ', to_user)
 
             // Only create if its not already cached
             if (typeof users[to_user] === 'undefined') {
                 // Store
                 users[to_user] = true
 
-                // Message
-                messages[to_user] = element
+                messages[to_user] = {}
+                messages[to_user][0] = element
+
+                //console.log('Created object for ', to_user)
 
                 // Create
                 PopulateUserList(to_user)
+            } else {
+                // Insert message
+                messages[to_user][i] = element
+
+               // console.log('Inserting for ', to_user, ' ', i, ' ', element)
             }
+
+            // Increase
+             i++;
         })
     }
 
     // Called when profiles are in
     function PopulateUserList(a) {
+        //console.log(messages)
         GeefProfielVanID(a).then(function (data) {
+           // console.log(a)
             // Create button
             var btn = $('#contacten').append("<li class='list-group-item chatitem'><p><button class='btn btn-primary' id='chatuser-" + data.id + "'>" + data.nickname + ' - ' + data.id + "</button></li>")
             // DoClick
             $('#chatuser-' + data.id).click(function () {
-                // Get the ID
-                var id = data.id;
+                // Clear
+                $('#berichten').empty()
 
-                for (let i = 0; i < data.length; i++) {
-                    console.log(' -> ', data[i])
+                // Insert them
+                for (let b in messages[a]) {
+                    $('#berichten').append('<div class="container mb-4 ' + (GeefGebruikerID() == messages[a][b].vanId ? 'bericht-from text-left' : 'bericht-to text-right') + '">' + messages[a][b].bericht + '</div>') 
                 }
-
             })
         })
+    }
+
+    // Stuur een bericht
+    function SendMessage(to, message) {
 
     }
 
