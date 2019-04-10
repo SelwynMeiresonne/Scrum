9b94fc9000f38df503170db8c4820239fac66504
DEV09 <dev09@ccdenovl.intra>
2019-04-10 11:42:45 +0200

    Kan berichten sturen/ontvangen

--- js/messenger.js
+++ js/messenger.js
@@ -24,6 +24,7 @@ $('document').ready(function () {
 
     let users = {}
     let messages = []
+    var selectedTarget = -1
     fetch(url + "?profielId=" + id)
         .then(function (response) { return response.json(); })
         .then(function (data) {
@@ -33,7 +34,7 @@ $('document').ready(function () {
         })
 
     function CreateConversations(data) {
-        
+
         let i = 0;
         data.forEach(element => {
             console.log(element)
@@ -58,11 +59,11 @@ $('document').ready(function () {
                 // Insert message
                 messages[to_user][i] = element
 
-               // console.log('Inserting for ', to_user, ' ', i, ' ', element)
+                // console.log('Inserting for ', to_user, ' ', i, ' ', element)
             }
 
             // Increase
-             i++;
+            i++;
         })
     }
 
@@ -70,7 +71,7 @@ $('document').ready(function () {
     function PopulateUserList(a) {
         //console.log(messages)
         GeefProfielVanID(a).then(function (data) {
-           // console.log(a)
+            // console.log(a)
             // Create button
             var btn = $('#contacten').append("<li class='list-group-item chatitem'><p><button class='btn btn-primary' id='chatuser-" + data.id + "'>" + data.nickname + ' - ' + data.id + "</button></li>")
             // DoClick
@@ -78,16 +79,86 @@ $('document').ready(function () {
                 // Clear
                 $('#berichten').empty()
 
+                // Update
+                selectedTarget = parseInt(a);
+
                 // Insert them
                 for (let b in messages[a]) {
-                    $('#berichten').append('<div class="container mb-4 ' + (GeefGebruikerID() == messages[a][b].vanId ? 'bericht-from text-left' : 'bericht-to text-right') + '">' + messages[a][b].bericht + '</div>') 
+                    $('#berichten').append('<div class="container mb-4 ' + (GeefGebruikerID() == messages[a][b].vanId ? 'bericht-from text-left' : 'bericht-to text-right') + '">' + messages[a][b].bericht + '</div>')
                 }
             })
         })
     }
 
+    function GetMessagesCount(id) {
+        let count = 0
+        for (let b in messages[id]) {
+            count++;
+        }
+
+        return count;
+    }
+
+    // Send button
+    $('#sendmessage').click(function () {
+        console.log('CCCCCCCCCC')
+        let text = $('input[name="bericht"]').val();
+
+        console.log(selectedTarget, selectedTarget >= 0)
+
+        // Meh
+        if (selectedTarget <= 0) { return; }
+
+        console.log('ya boi')
+
+        // Send
+        SendMessage(selectedTarget, text)
+    })
+
     // Stuur een bericht
     function SendMessage(to, message) {
+        console.log('SENDING ', to, ' -> ', message)
+        let url = ROOT_URL + '/bericht/post.php';
+
+        let data = {
+            vanId: GeefGebruikerID(),
+            naarId: to,
+            bericht: message,
+            status: "verzonden"
+        }
+
+        var request = new Request(url, {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: new Headers({
+                'Content-Type': 'application/json'
+            })
+        });
+
+        fetch(request)
+            .then(function (resp) { return resp.json(); })
+            .then(function (data) { 
+                if (data.message = "Bericht werd aangemaakt.") {
+                    // Into the stack
+                    var len = GetMessagesCount(selectedTarget)
+
+                    console.log(len)
+
+                    messages[selectedTarget][len] = {
+                        vanId: '' + GeefGebruikerID(),
+                        naarId: '' + selectedTarget,
+                        partnerId: '' + selectedTarget,
+                        bericht: message,
+
+                    }
+
+                    console.log(messages[selectedTarget], len)
+
+                    $('#berichten').append('<div class="container mb-4 ' + (GeefGebruikerID() == messages[selectedTarget][len].vanId ? 'bericht-from text-left' : 'bericht-to text-right') + '">' + message + '</div>')
+
+                }
+            })
+            .catch(function (error) { console.log(error); });
 
     }
 
