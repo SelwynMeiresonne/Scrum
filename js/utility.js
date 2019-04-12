0291d10eccb5a117bca7d85baa9a8542b6f59e7f
DEV09 <dev09@ccdenovl.intra>
2019-04-12 11:34:37 +0200

    COINS COINS COINS FIXED

--- js/utility.js
+++ js/utility.js
@@ -28,39 +28,56 @@ function GetBasePath() {
 // Maak de navigatie als je ingelogd bent
 $('navigationElements').ready(function () {
     var isRoot = window.location.href.endsWith('index.html');
+    // Altijd update eerst want standaard is het 0 (of 1)
+    var lastcoins = -1
 
-    if (IsIngelogd()) {
-        // Toon amount
-        GeefProfielVanID(GeefGebruikerID()).then(function (data) {
-            for (let i in NAVIGATION) {
-                var url = NAVIGATION[i].Pagina
+    // Update
+    function UpdateNav() {
+        if (IsIngelogd()) {
+            // Toon amount
+            GeefProfielVanID(GeefGebruikerID()).then(function (data) {
+                console.log(lastcoins, data.lovecoins)
+                if (parseInt(data.lovecoins) != parseInt(lastcoins)) {
+                    $('#navigationElements').empty()
+                    
+                    // yet
+                    lastcoins = data.lovecoins
 
-                if (!isRoot) {
-                    if (NAVIGATION[i].Naam != "Logout") {
-                        url = url.substring(url.lastIndexOf('/') + 1, url.length)
-                    } else {
-                        url = "../" + url
+                    for (let i in NAVIGATION) {
+                        var url = NAVIGATION[i].Pagina
+
+                        if (!isRoot) {
+                            if (NAVIGATION[i].Naam != "Logout") {
+                                url = url.substring(url.lastIndexOf('/') + 1, url.length)
+                            } else {
+                                url = "../" + url
+                            }
+                        }
+
+                        if (NAVIGATION[i].Naam == "Love Coins") {
+                            // Modify
+                            if (data.lovecoins > 0) {
+                                NAVIGATION[i].Naam += ' <span style="color:#ffbf00">(' + data.lovecoins + ')</span>'
+                            }
+                        }
+                    
+                        var btn = $('<li class="nav-item pl-3"><a class="nav-link" href="' + url + '">' + NAVIGATION[i].Naam + '</a></li>').appendTo('#navigationElements')
+
+                        if (NAVIGATION[i].Naam == "Logout") {
+                            btn.click(function () {
+                                Logout()
+                                Redirect('index.html')
+                            })
+                        }
                     }
                 }
+            })
+        }
 
-                if (NAVIGATION[i].Naam == "Love Coins") {
-                    // Modify
-                    if (data.lovecoins > 0) {
-                        NAVIGATION[i].Naam += ' <span style="color:#ffbf00">(' + data.lovecoins + ')</span>'
-                    }
-                }
-            
-                var btn = $('<li class="nav-item pl-3"><a class="nav-link" href="' + url + '">' + NAVIGATION[i].Naam + '</a></li>').appendTo('#navigationElements')
-
-                if (NAVIGATION[i].Naam == "Logout") {
-                    btn.click(function () {
-                        Logout()
-                        Redirect('index.html')
-                    })
-                }
-            }
-        })
+        setTimeout(UpdateNav, 1000 * 1)
     }
+
+    UpdateNav()
 })
 
 
